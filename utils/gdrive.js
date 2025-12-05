4b2776918a2ce31c63781e14e286c07e66a3e6d0
Ankush007669 <ankush214500@gmail.com>
2025-12-05 12:12:24 +0530

    Update68 gdrive.js

--- utils/gdrive.js
+++ utils/gdrive.js
@@ -1,21 +1,42 @@
 import fs from "fs";
 import fetch from "node-fetch";
-import config from "../config.js";
 
 export function extractFileId(link) {
-  const match = link.match(/\/d\/(.*?)\//);
-  return match ? match[1] : null;
+    const match = link.match(/\/d\/([^\/]+)/);
+    return match ? match[1] : null;
 }
 
-export async function downloadGDrive(fileId, outPath) {
-  const url = config.worker_url + "chunk?fileId=" + fileId;
+export async function downloadGDrive(fileId, outputPath) {
+    const url = `https://drive.google.com/uc?export=download&id=${fileId}`;
 
-  const res = await fetch(url);
-  const stream = fs.createWriteStream(outPath);
+    const res = await fetch(url);
 
-  return new Promise((resolve) => {
-    res.body.pipe(stream);
-    res.body.on("end", resolve);
-  });
+    // If Google blocks direct download → bypass with confirm token
+    if (res.headers.get("content-type").includes("text/html")) {
+        const html = await res.text();
+        const token = html.match(/confirm=([^&]+)/)?.[1];
+
+        if (!token) throw new Error("Google Drive confirmation token not found");
+
+        const bypassUrl =
+            `https://drive.google.com/uc?export=download&confirm=${token}&id=${fileId}`;
+
+        return await downloadStream(bypassUrl, outputPath);
+    }
+
+    return await downloadStream(url, outputPath);
 }
 
+async function downloadStream(url, outputPath) {
+    const res = await fetch(url);
+
+    if (!res.ok) throw new Error("GDrive download failed");
+
+    const fileStream = fs.createWriteStream(outputPath);
+
+    return new Promise((resolve, reject) => {
+        res.body.pipe(fileStream);
+        res.body.on("error", reject);
+        fileStream.on("finish", resolve);
+    });
+}
