90dbab461529cbda996154cd8f6ad892faa8d5de
Ankush007669 <ankush214500@gmail.com>
2025-12-05 11:43:55 +0530

    Update2 gdrive.js

--- utils/gdrive.js
+++ utils/gdrive.js
@@ -1,37 +1,20 @@
-import fetch from "node-fetch";
 import fs from "fs";
+import fetch from "node-fetch";
 import config from "../config.js";
 
-// Extract Google Drive File ID
 export function extractFileId(link) {
-    const match = link.match(/\/d\/(.*?)\//);
-    return match ? match[1] : null;
+  const match = link.match(/\/d\/(.*?)\//);
+  return match ? match[1] : null;
 }
 
-// Sequential Chunk Downloader via Worker
-export async function downloadGDrive(fileId, filePath) {
-    const chunkSize = 5 * 1024 * 1024; // 5MB safe chunk
-    let start = 0;
-    let end = chunkSize - 1;
-    let done = false;
+export async function downloadGDrive(fileId, outPath) {
+  const url = config.worker_url + "chunk?fileId=" + fileId;
 
-    const stream = fs.createWriteStream(filePath);
+  const res = await fetch(url);
+  const stream = fs.createWriteStream(outPath);
 
-    while (!done) {
-        const url = `${config.worker_url}chunk?fileId=${fileId}&start=${start}&end=${end}`;
-        const res = await fetch(url);
-
-        if (!res.ok) throw new Error("Worker chunk fetch failed " + res.status);
-
-        const buf = Buffer.from(await res.arrayBuffer());
-        stream.write(buf);
-
-        if (buf.length < chunkSize) done = true;
-
-        start += chunkSize;
-        end += chunkSize;
-    }
-
-    stream.end();
-    return true;
+  return new Promise((resolve) => {
+    res.body.pipe(stream);
+    res.body.on("end", resolve);
+  });
 }
