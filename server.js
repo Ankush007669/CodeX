844b1bf9039b83a587649af9522aae7d9dac2c54
Ankush007669 <ankush214500@gmail.com>
2025-12-05 11:45:06 +0530

    Update2 server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from "express";
-import uploadRouter from "./routes/upload.js";
+import uploadRoute from "./routes/upload.js";
 
 const app = express();
 app.use(express.json());
-app.use("/api/upload", uploadRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("🔥 Mirror Engine Running on PORT " + PORT));
+app.use("/api/upload", uploadRoute);
+
+app.listen(10000, () => console.log("🔥 VPS API Running"));
